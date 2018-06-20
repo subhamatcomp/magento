2e2248316d3438850f1aac7a0871d1f2c7c858ac
Subham Kumar <subham.kumar@infoprolearning.com>
2018-06-20 15:09:33 +0530

    gitignore

---
+++ magento2/app/etc/env.php
@@ -0,0 +1,49 @@
+<?php
+return [
+  'backend' => [
+    'frontName' => 'admin'
+  ],
+  'crypt' => [
+    'key' => 'e72263667b44def71aab2d558956a337'
+  ],
+  'db' => [
+    'table_prefix' => '',
+    'connection' => [
+      'default' => [
+        'host' => 'localhost',
+        'dbname' => 'mage2',
+        'username' => 'root',
+        'password' => '',
+        'active' => '1'
+      ]
+    ]
+  ],
+  'resource' => [
+    'default_setup' => [
+      'connection' => 'default'
+    ]
+  ],
+  'x-frame-options' => 'SAMEORIGIN',
+  'MAGE_MODE' => 'default',
+  'session' => [
+    'save' => 'files'
+  ],
+  'cache_types' => [
+    'config' => 1,
+    'layout' => 1,
+    'block_html' => 1,
+    'collections' => 1,
+    'reflection' => 1,
+    'db_ddl' => 1,
+    'eav' => 1,
+    'customer_notification' => 1,
+    'config_integration' => 1,
+    'config_integration_api' => 1,
+    'full_page' => 1,
+    'translate' => 1,
+    'config_webservice' => 1
+  ],
+  'install' => [
+    'date' => 'Wed, 30 May 2018 07:38:41 +0000'
+  ]
+];
